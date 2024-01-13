341a2c9bdd77f7e790cedb9000fa0c8da00ad3a7
Abdul Ghaffar <ghaffarbajwa9@gmail.com>
2024-01-13 19:00:23 +0500

    seed file is updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,40 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.create!([
+    {
+    name: "Ali",
+    email: "email@mail.com",
+    password_digest: "123"
+},
+{
+    name: "Ahmad",
+    email: "ahmad@mail.com",
+    password_digest: "123"
+}
+])
+
+Account.create!([
+    {
+        name: "fast",
+        balance: 2341,
+        user_id: 1
+    },
+    {
+        name: "student",
+        balance: 231,
+        user_id: 2
+    }
+])
+
+Payment.create!([
+    {
+        amount: 45,
+        date: Date.now(),
+        user_id: 1,
+        account_id: 1,
+    },
+    {
+        amount: 234,
+        date: Date.now(),
+        user_id: 2,
+        account_id: 2,
+    }
+])
